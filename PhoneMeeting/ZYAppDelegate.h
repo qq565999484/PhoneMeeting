bfb927cb54f30ce5955f80c0b99c110907590c2f
lizheng <719446602@qq.com>
2015-06-03 15:27:15 +0800

    Initial Commit

---
+++ PhoneMeeting/ZYAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  ZYAppDelegate.h
+//  PhoneMeeting
+//
+//  Created by apple on 15/6/3.
+//  Copyright (c) 2015年 ZhiYou. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ZYAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ PhoneMeeting/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  PhoneMeeting
+//
+//  Created by apple on 15/6/3.
+//  Copyright (c) 2015年 ZhiYou. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "ZYAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([ZYAppDelegate class]));
+    }
+}
